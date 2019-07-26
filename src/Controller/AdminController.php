bcac2adc830c1db296461c5f598ecb5e8d4e570e
lRejou <louisrejou@yahoo.fr>
2019-07-26 16:39:47 +0200

    change admin route and display videos from database (video admin)

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,29 +1,50 @@
 <?php
 namespace App\Controller;
-use Symfony\Component\HttpFoundation\Response;
-use Twig\Environment;
 
-class AdminController 
+use Twig\Environment;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\VideoUser;
+use App\Repository\VideoUserRepository;
+
+/**
+ * @Route("/admin")
+ */
+class AdminController extends AbstractController
 {
     /**
      * @var Environment
      */
     private $twig;
 
-
     public function __construct(Environment $twig)
     {
         $this->twig = $twig;
     }
 
-    public function index(): Response
+    /**
+     * @Route("/", name="admin_index", methods={"GET"})
+     */
+    public function index(VideoUserRepository $videoUserRepository): Response
     {
-        return new Response($this->twig->render('admin/home.html.twig'));
+        return new Response($this->twig->render('admin/home.html.twig', [
+            'videosUser' => $videoUserRepository->findAll()
+        ]));
     }
 
-    public function video(): Response
+    /**
+     * @Route("/video/{id}", name="admin_video", methods={"GET"})
+     */
+    public function video(VideoUserRepository $videoUserRepository, VideoUser $videoUser): Response
     {
-        return new Response($this->twig->render('admin/video.html.twig'));
+        return $this->render('admin/video.html.twig', [
+            'video' => $videoUserRepository->findOneBy(
+                ['id' => $videoUser->getId()]
+            )
+        ]);
+        //return new Response($this->twig->render('admin/video.html.twig'));
     }
 
 }
