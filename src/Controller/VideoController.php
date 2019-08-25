8dc801af4b75b1d38e6662f1eff63f97c07d8e4f
lrejou <lrejou@u-bordeaux.fr>
2019-08-25 18:00:22 +0200

    fix video notFound

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -29,10 +29,15 @@ class VideoController extends AbstractController
     /**
      * @Route("/{id}", name="video_show", methods={"GET"})
      */
-    public function show(VideoRepository $videoRepository, Video $video){
+    public function show(VideoRepository $videoRepository){
+        $tab = explode('/', $_SERVER['REQUEST_URI']);
+        if( $videoRepository->findOneBy(['id' => $tab[2]]) != NULL){
+            $video360 = $videoRepository->findOneBy(['id' => $tab[2]]);
+            return $this->render('video/index.html.twig', ['video' => $video360]);
+        }
+        else{
+            return $this->render('video/Notfound.html.twig');
+        }
 
-        $video360 = $videoRepository->findOneBy(['id' => $video->getId()]);
-
-        return $this->render('video/index.html.twig', ['video' => $video]);
     }
 }
